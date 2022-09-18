01518b816eeb280af6fc32e9d0ba6031c734fbe7
AnshumanDhara <anshumandhara123@gmail.com>
2022-09-19 00:25:56 +0530

    commit

--- Structures/Stack.java
+++ Structures/Stack.java
@@ -1,1 +1,45 @@
-import java.util.*;
+import javax.sound.sampled.SourceDataLine;
+
+class IntStack
+{
+    int size,top;
+    int stack[];
+    IntStack(int n)
+    {
+        size=n;
+        top=-1;
+        stack=new int[n];
+    }
+    void push(int a)
+    {
+        if(!isFull())
+        {
+            stack[++top]=a;
+            System.out.println(stack[top]+" was pushed");
+        }
+        else
+        System.out.println("Stack Overflow!!");
+    }
+    void pop()
+    {
+        if(!isEmpty())
+        {
+            System.out.println(peek()+" was popped");
+            --top;
+        }
+        else
+        System.out.println("Stack Underflow!!");
+    }
+    int peek()
+    {
+        return stack[top];
+    }
+    boolean isEmpty()
+    {
+        return top==-1?true:false;
+    }
+    boolean isFull()
+    {
+        return top==size-1?true:false;
+    }
+}
